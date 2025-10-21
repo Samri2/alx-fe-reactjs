20bc72713dd92a841945b32aad0ac9433f51857a
samri2 <wsamrawit2@gmail.com>
2025-10-21 20:49:37 +0300

    new vite react app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,15 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-// src/App.jsx
 import WelcomeMessage from './components/WelcomeMessage';
-
+import Header from './header'
+import MainContent from './MainContent';
+import Footer from './footer'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
   
       <div className="App">
+        <Header />
+      <MainContent />
+      <Footer />
       <WelcomeMessage />  {/* Include the WelcomeMessage component */}
     </div>
   )
