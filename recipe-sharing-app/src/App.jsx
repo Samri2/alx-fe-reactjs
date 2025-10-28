6bb73336fd92135573fd3d21c0ecf564fab7054a
samri2 <wsamrawit2@gmail.com>
2025-10-28 21:19:59 +0300

    zustand

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,23 @@
+// src/App.jsx
 import React from "react";
-import SearchBar from "./components/SearchBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
+import RecipeDetail from "./components/RecipeDetail";
 
 const App = () => {
   return (
-    <div className="container mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-4">Recipe Sharing App 🍳</h1>
-      <SearchBar />
-      <RecipeList />
-    </div>
+    <Router>
+      <div className="container mx-auto p-6">
+        <h1 className="text-2xl font-bold mb-4">Recipe Sharing App 🍳</h1>
+
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/add" element={<AddRecipeForm />} />
+          <Route path="/recipes/:id" element={<RecipeDetail />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
