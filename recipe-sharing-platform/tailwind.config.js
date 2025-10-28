9794635c6a63b047c21f30d452087c187e8ef305
samri2 <wsamrawit2@gmail.com>
2025-10-28 23:08:32 +0300

    tailwind

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,10 @@
 // tailwind.config.js
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
-  darkMode: false, // 'media' or 'class' if needed
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Scan all JS/TS/JSX/TSX files in src
+    './public/index.html'          // Include the main HTML file
+  ],
+  darkMode: false, // or 'media' / 'class' if needed
   theme: {
     extend: {},
   },
