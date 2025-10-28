be27220ba89cf0218ae75e3715fdafc74c063613
samrawit <wsamrawit2@gmail.com>
2025-10-28 09:37:22 +0300

    new react

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: '#f0f8ff', padding: '20px', textAlign: 'center' }}>
+      <p style={{ fontSize: '1.2rem' }}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
